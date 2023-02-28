9301ea12655154380281dede734e2f41c4b6c6d5
Henry-Eg <107496624+Henry-Egboh@users.noreply.github.com>
2023-02-28 18:30:06 +0100

    Added content to the showcase content

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,12 @@ import { Container, Button } from "react-bootstrap";
 const Header = () => {
     return (
         <header>
-             <div className="bg-secondary py-5">
-            <Container className="py-5">
-                    <h2>Maxmel Tech</h2>
-                    <h5>What we do reveals your expectation</h5>
-                    <Button className="color-btn color-text-sub">Benefits</Button>
-                    <Button className="color-btn color-text-sub">Contact Us</Button>
+             <div className="showcase-bg py-5">
+            <Container className="py-5 my-5">
+                    <h1 className='color-text'>Max<span className='color-text-sub'>mel</span><span className="text-dark"> Tech</span></h1>
+                    <h3 className="text-dark mt-3 mb-5">What we do reveals your expectation</h3>
+                    <Button className="color-btn color-text-sub btn-outline-light">Benefits</Button>
+                    <Button className="color-btn color-text-sub btn-outline-light ms-3">Contact Us</Button>
                
             </Container>
             </div>
